9c8dc0ee7272d1a36f5ebde5b4baef28a970947e
Vinicius Gati <viniciusgati@gmail.com>
2021-08-30 16:04:09 -0300

    First commit

---
+++ src/main/java/live/gatisoft/storerequestscontrol/StoreRequestsControlApplication.java
@@ -0,0 +1,13 @@
+package live.gatisoft.storerequestscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreRequestsControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreRequestsControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/gatisoft/storerequestscontrol/StoreRequestsControlApplicationTests.java
@@ -0,0 +1,13 @@
+package live.gatisoft.storerequestscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreRequestsControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
