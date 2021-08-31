2d34d5a507ccdcf70b880cee5e3a6b27abe1504b
Vinicius Gati <viniciusgati@gmail.com>
2021-08-31 15:40:59 -0300

    testing if attributes are persisted correctly

--- src/test/java/live/gatisoft/storerequestscontrol/common/repository/UserRepositoryTest.java
+++ src/test/java/live/gatisoft/storerequestscontrol/common/repository/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package live.gatisoft.storerequestscontrol.common.repository;
 
 import live.gatisoft.storerequestscontrol.common.schema.User;
+import live.gatisoft.storerequestscontrol.security.services.BCryptService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +24,8 @@ class UserRepositoryTest {
     }
 
     @Test
-    @DisplayName("Test if a new user is persisted successfully")
-    public void testUserPersistenceAndDelte() {
+    @DisplayName("Test that a user is deleted correctly")
+    public void testUserPersistenceAndDelete() {
         User user = new User();
         userRepository.save(user);
         assertTrue(user.getId() > 0);
@@ -32,6 +33,21 @@ class UserRepositoryTest {
         assertEquals(0, userRepository.count());
     }
 
+    @Test
+    @DisplayName("Test if a new user is persisted successfully and attributes to")
+    public void testUserPersistenceOfAttributes() {
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setDocument("11144477735");
+        user.setPasswordResetToken("resetToken");
+        user.setPassword("1234");
+        userRepository.save(user);
 
+        User persisted = userRepository.findById(user.getId()).get();
+        assertEquals(user.getDocument(), persisted.getDocument());
+        assertEquals(user.getEmail(), persisted.getEmail());
+        assertEquals(user.getPasswordResetToken(), persisted.getPasswordResetToken());
+        assertTrue(BCryptService.passwordMatches("1234", user.getEncryptedPassword()));
+    }
 
 }
